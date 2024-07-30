49227c41847bbd1402a628c3313ca1fc941712d9
shivashankarbejjanki <162316443+shivashankarbejjanki@users.noreply.github.com>
2024-07-30 12:03:18 +0530

    Add files via upload

---
+++ Marks.py
@@ -0,0 +1,11 @@
+marks = input("marks: ")
+if(marks >= "90"):
+    print("Grade A")
+elif(marks >= "80"):
+    print("Grade B")
+elif(marks >= "70"):
+    print("Grade C")
+elif(marks >= "50"):
+    print("pass")
+else:
+    print("Fail")
---
+++ Traffic.py
@@ -0,0 +1,9 @@
+light = input("light colour:")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("Ready")
+elif(light == "green"):
+    print("Go")
+else:
+    print("lights are not working")
