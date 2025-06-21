89f2bcd8e324bbc3b29d232bac525de82b909602
shiva shankara chary Bejjanki <162316443+shivashankarbejjanki@users.noreply.github.com>
2025-06-21 10:38:16 +0530

    Add files via upload

--- Datatype.py
+++ Datatype.py
@@ -1,2 +1,30 @@
 x=10
-print(type(x))
+print(type(x))      # Printing the data type of the variable x;
+
+# some Built-in Data Types;
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneTyp
+
+# EXAMPLES;
+
+x = "Hello World"	                            # str	
+x = 20	                                        # int	
+x = 20.5	                                    # float	
+x = 1j	                                        # complex	
+x = ["apple", "banana", "cherry"]	            # list	
+x = ("apple", "banana", "cherry")	            # tuple	
+x = range(6)	                                # range	
+x = {"name" : "John", "age" : 36}	            # dict	
+x = {"apple", "banana", "cherry"}	            # set	
+x = frozenset({"apple", "banana", "cherry"})	# frozenset	
+x = True	                                    # bool	
+x = b"Hello"	                                # bytes	
+x = bytearray(5)	                            # bytearray	
+x = memoryview(bytes(5))	                    # memoryview	
+x = None	                                    # NoneType	
