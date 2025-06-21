b3fdbb2d7ace0841c60688cf7906525b001adb1a
shiva shankara chary Bejjanki <162316443+shivashankarbejjanki@users.noreply.github.com>
2025-06-21 10:53:33 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,10 @@
+# Strings
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+# 'shiva' is the same as "shiva"
+
+#You can use double or single quotes:
+
+print("Hello")
+print('Hello')
+
