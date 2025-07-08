421e3272eed1d319b78ff98506ce513ddd3bb7c9
shiva shankara chary Bejjanki <162316443+shivashankarbejjanki@users.noreply.github.com>
2025-07-08 13:43:30 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,26 @@
+"While loop"
+"While loop is used for execution of statements untill the given condition is satisfied"
+
+i = 1
+while i < 10:
+    print(i)
+    i += 1;
+
+a = 11
+while a <= 15:
+    print(a)
+    a += 2;
+
+
+
+"The break Statement"
+"With the break statement we can stop the loop even if the while condition is true:"
+
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
